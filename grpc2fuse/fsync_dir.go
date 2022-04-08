3e14d24ef844ffd9861538f025ab941ed7764b5b
炽天 <hanxin.hx@alibaba-inc.com>
2022-04-08 16:15:55 +0800

    grpc2fuse: add FsyncDir()
    Signed-off-by: 炽天 <hanxin.hx@alibaba-inc.com>

---
+++ grpc2fuse/fsync_dir.go
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 Han Xin, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package grpc2fuse
+
+import (
+	"github.com/chiyutianyi/grpcfuse/pb"
+
+	"github.com/hanwen/go-fuse/v2/fuse"
+	log "github.com/sirupsen/logrus"
+)
+
+func (fs *fileSystem) FsyncDir(cancel <-chan struct{}, input *fuse.FsyncIn) (code fuse.Status) {
+	ctx := newContext(cancel, &input.InHeader)
+	defer releaseContext(ctx)
+
+	res, err := fs.client.FsyncDir(ctx, &pb.FsyncRequest{
+		Header:     toPbHeader(&input.InHeader),
+		Fh:         input.Fh,
+		FsyncFlags: input.FsyncFlags,
+		Padding:    input.Padding,
+	}, fs.opts...)
+
+	if err != nil {
+		log.Errorf("FsyncDir: %v", err)
+		return fuse.EIO
+	}
+
+	return fuse.Status(res.Status.GetCode())
+}
